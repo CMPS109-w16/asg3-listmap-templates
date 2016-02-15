9d2a34a8c6f958f9b7325a6694a84b12625f9a91
ColStaR <starshootr@gmail.com>
2016-02-15 00:50:06 -0800

    Added printline and line number functionality.

--- main.cpp
+++ main.cpp
@@ -39,21 +39,27 @@ string trim(string line) {
 
    start = line.find_first_not_of(" ");
    end = line.find_last_not_of(" ");
-//    Single-character lines need to be handled specially.
-//   if (start == end && start != 0 && end != 0) {
-//      if (line == "#") {
-//         return "#";
-//      } else if (line == "=") {
-//         return "=";
-//      } else if (line == "") {
-//         return "";
-//      }
-//   }
+//    Some single-character lines need to be handled specially.
+   if (start == end && start != 0 && end != 0) {
+      if (line == "#") {
+         return "#";
+      } else if (line == "=") {
+         return "=";
+      } else if (line == "") {
+         return "";
+      }
+   }
    line = line.substr(start, end - start + 1);
    return line;
 }
 
-void format_line(string line) {
+// Prints out a line of output, consisting of the file name, the line
+// number, and the string of output.
+void print_line(const string file_name, int& line_num, string line_str) {
+   cout << file_name << ": " << line_num << ": " << line_str << endl;
+}
+
+void format_line(string title, int line_num, string line) {
    str_str_map test;
    string first {};
    string second {};
@@ -69,7 +75,7 @@ void format_line(string line) {
 
    // If there's no = sign at all...
    if (line.find_first_of("=") == string::npos) {   // case: key
-      cout << "Search for Keys and stuff";
+      print_line(title, line_num, "Search for Keys and stuff");
    } else {
       // If there is an = sign somewhere, first find where it is.
       equal_sign_pos = line.find_first_of("=");
@@ -81,10 +87,10 @@ void format_line(string line) {
          second = trim(second);
          // If = is alone without values, show all keys and values.
          if (second == "") {      // case: =
-            cout << "Show Everything and stuff...";
+            print_line(title, line_num, "Show Everything and stuff...");
          // If = is followed by a value, show the keys with that value.
          } else {       // case: = value
-            cout << "Find Keys from Values and stuff...";
+            print_line(title, line_num, "Find Keys from Values and stuff...");
          }
       // If equals is not the first character...
       } else {
@@ -94,13 +100,13 @@ void format_line(string line) {
          second = trim(second);
          // If there is a key followed by =, show the value of that key.
          if (second == "") {    // case: key =
-            cout << "Show Value for Key and stuff...";
+            print_line(title, line_num, "Show Value for Key and stuff...");
          // Final case is a key, =, and a value in that order.
          // Insert the key and value into the map.
          } else {    // case: key = value
             str_str_pair pair(first, second);
             test.insert(pair);
-            cout << pair << " was inserted and stuff!" << endl;
+            print_line(title, line_num, first + " = " + second);
          }
       }
       // Debugging prints. Remove later.
@@ -109,36 +115,42 @@ void format_line(string line) {
    }
 }
 
-int main (int argc, char** argv) {
-   sys_info::set_execname (argv[0]);
-   scan_options (argc, argv);
+int main(int argc, char** argv) {
+   sys_info::set_execname(argv[0]);
+   scan_options(argc, argv);
    string line;
    // If no arguments are specified when executing the program,
    // the program will take cin inputs via command line.
-   if(argc == optind){
-      for(;;) {
+   if (argc == optind) {
+      int line_num = 1;
+      for (;;) {
          getline(cin, line);
-         if(cin.eof()) break;
-         format_line(line);
+         if (cin.eof()) break;
+         format_line("-", line_num, line);
+         ++line_num;
       }
    }
    // If arguments are specified when executing the program,
    // the program will treat the arguments as file names, and input the
    // lines within those files as command inputs.
    else {
-      for(char** argp = &argv[optind]; argp != &argv[argc]; ++argp){
-          try{
+      for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
+         int line_num = 1;
+         try {
             ifstream myfile(*argp);
-            if(!myfile)
+            if (!myfile)
                throw processing_error("No such file or directory");
-            while(getline(myfile, line)) format_line(line);
+            while (getline(myfile, line)) {
+               format_line(*argp, line_num, line);
+               ++line_num;
+            }
             myfile.close();
-         }catch(processing_error& error){
+         } catch (processing_error& error) {
             complain() << error.what() << endl;
          }
       }
    }
-cout << "EXIT_SUCCESS" << endl;
-return EXIT_SUCCESS;
+   cout << "EXIT_SUCCESS" << endl;
+   return EXIT_SUCCESS;
 }
 

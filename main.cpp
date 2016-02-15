565de700765c1a8428fd9e54b6281c14d604a4dc
ColStaR <starshootr@gmail.com>
2016-02-15 00:20:25 -0800

    Added additional comments to main.cpp.

--- main.cpp
+++ main.cpp
@@ -32,75 +32,78 @@ void scan_options (int argc, char** argv) {
    }
 }
 
+// Removes trailing whitespace from the beginning and end of a string.
 string trim(string line) {
    size_t start = 0;
    size_t end = 0;
 
    start = line.find_first_not_of(" ");
    end = line.find_last_not_of(" ");
-   if (start == end && start != 0 && end != 0) {
-      if (line == "#") {
-         return "#";
-      } else if (line == "=") {
-         return "=";
-      } else if (line == "") {
-         return "";
-      }
-   }
+//    Single-character lines need to be handled specially.
+//   if (start == end && start != 0 && end != 0) {
+//      if (line == "#") {
+//         return "#";
+//      } else if (line == "=") {
+//         return "=";
+//      } else if (line == "") {
+//         return "";
+//      }
+//   }
    line = line.substr(start, end - start + 1);
    return line;
 }
 
 void format_line(string line) {
    str_str_map test;
-   string first;
-   string second;
+   string first {};
+   string second {};
    size_t equal_sign_pos { };
 
+   // Doesn't do anything for empty line commands.
    if (line == "") {    // case: <whitespace>
       return;
+      // Doesn't do anything for comments either.
    } else if (line.find_first_of("#") == 0) {   // case: #
       return;
    }
 
-   // If there's no = sign at all
+   // If there's no = sign at all...
    if (line.find_first_of("=") == string::npos) {   // case: key
       cout << "Search for Keys and stuff";
-
-      auto itor = test.find(line);
-      if (itor != test.end())
-         cout << itor->first << " = " << itor->second << endl;
-      else
-         cout << line << ": key not found" << endl;
-
    } else {
-      // If there is an = sign somewhere
+      // If there is an = sign somewhere, first find where it is.
       equal_sign_pos = line.find_first_of("=");
 
-      // If the equals is the first character.
+      // If the equals is the first character...
       if (equal_sign_pos == 0) {
          first = "";
          second = line.substr(equal_sign_pos + 1);
          second = trim(second);
+         // If = is alone without values, show all keys and values.
          if (second == "") {      // case: =
             cout << "Show Everything and stuff...";
+         // If = is followed by a value, show the keys with that value.
          } else {       // case: = value
             cout << "Find Keys from Values and stuff...";
          }
-      } else {    // Equals is not the first character
+      // If equals is not the first character...
+      } else {
          first = line.substr(0, equal_sign_pos);
          second = line.substr(equal_sign_pos + 1);
          first = trim(first);
          second = trim(second);
-
+         // If there is a key followed by =, show the value of that key.
          if (second == "") {    // case: key =
             cout << "Show Value for Key and stuff...";
+         // Final case is a key, =, and a value in that order.
+         // Insert the key and value into the map.
          } else {    // case: key = value
             str_str_pair pair(first, second);
             test.insert(pair);
             cout << pair << " was inserted and stuff!" << endl;
          }
       }
+      // Debugging prints. Remove later.
       str_str_pair pair(first, second);
       cout << pair << endl;
    }
@@ -110,6 +113,8 @@ int main (int argc, char** argv) {
    sys_info::set_execname (argv[0]);
    scan_options (argc, argv);
    string line;
+   // If no arguments are specified when executing the program,
+   // the program will take cin inputs via command line.
    if(argc == optind){
       for(;;) {
          getline(cin, line);
@@ -117,6 +122,9 @@ int main (int argc, char** argv) {
          format_line(line);
       }
    }
+   // If arguments are specified when executing the program,
+   // the program will treat the arguments as file names, and input the
+   // lines within those files as command inputs.
    else {
       for(char** argp = &argv[optind]; argp != &argv[argc]; ++argp){
           try{

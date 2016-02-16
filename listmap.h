ba6132b1ff378f725438d44477c0e6b2eda4fa34
ColStaR <starshootr@gmail.com>
2016-02-15 18:12:50 -0800

    Merged for File System additions, command functions key and =.

--- listmap.h
+++ listmap.h
@@ -33,11 +33,11 @@ class listmap {
       listmap& operator= (const listmap&);
       ~listmap();
       iterator insert (const value_type&);
-      iterator find (const key_type&) const;
+      iterator find (const key_type&);
       iterator erase (iterator position);
       iterator begin() { return anchor()->next; }
       iterator end() { return anchor(); }
-      bool empty() const { return begin() == end(); }
+      bool empty() { return begin() == end(); }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -15,67 +15,62 @@ using namespace std;
 using str_str_map = listmap<string,string>;
 using str_str_pair = str_str_map::value_type;
 
-void scan_options (int argc, char** argv) {
+void scan_options(int argc, char** argv) {
    opterr = 0;
    for (;;) {
-      int option = getopt (argc, argv, "@:");
+      int option = getopt(argc, argv, "@:");
       if (option == EOF) break;
       switch (option) {
-         case '@':
-            traceflags::setflags (optarg);
-            break;
-         default:
-            complain() << "-" << (char) optopt << ": invalid option"
-                       << endl;
-            break;
+      case '@':
+         traceflags::setflags(optarg);
+         break;
+      default:
+         complain() << "-" << (char) optopt << ": invalid option"
+                  << endl;
+         break;
       }
    }
 }
 
-// Removes trailing whitespace from the beginning and end of a string.
+//Removes trailing whitespace from the beginning and end of a string.
 string trim(string line) {
    size_t start = 0;
    size_t end = 0;
 
    start = line.find_first_not_of(" ");
    end = line.find_last_not_of(" ");
-//    Some single-character lines need to be handled specially.
-   if (start == end && start != 0 && end != 0) {
-      if (line == "#") {
-         return "#";
-      } else if (line == "=") {
-         return "=";
-      } else if (line == "") {
-         return "";
-      }
-   }
+   //Some single-character lines need to be handled specially.
+
+   if (line == "") return "";    // Prevents out of range error.
    line = line.substr(start, end - start + 1);
    return line;
 }
 
 // Prints out a line of output, consisting of the file name, the line
 // number, and the string of output.
-void print_line(const string file_name, int& line_num, string line_str) {
+void print_line(const string file_name, int& line_num,
+         string line_str) {
    cout << file_name << ": " << line_num << ": " << line_str << endl;
 }
 
-void format_line(string title, int line_num, string line) {
-   str_str_map test;
-   string first {};
-   string second {};
+void format_line(string title, int line_num, string line,
+         str_str_map& test) {
+   string first { };
+   string second { };
    size_t equal_sign_pos { };
-
-   // Doesn't do anything for empty line commands.
-   if (line == "") {    // case: <whitespace>
-      return;
-      // Doesn't do anything for comments either.
-   } else if (line.find_first_of("#") == 0) {   // case: #
-      return;
-   }
+   str_str_map::iterator curr;
 
    // If there's no = sign at all...
-   if (line.find_first_of("=") == string::npos) {   // case: key
-      print_line(title, line_num, "Search for Keys and stuff");
+   //case: key
+   if (line.find_first_of("=") == string::npos) {
+      curr = test.find(line);
+      if (curr == test.end())
+         cout << line << ": key not found" << endl;
+      else {
+         print_line(title, line_num, line);
+         cout << curr->first << " = " << curr->second << endl;
+      }
+      //cases =, =value
    } else {
       // If there is an = sign somewhere, first find where it is.
       equal_sign_pos = line.find_first_of("=");
@@ -86,32 +81,41 @@ void format_line(string title, int line_num, string line) {
          second = line.substr(equal_sign_pos + 1);
          second = trim(second);
          // If = is alone without values, show all keys and values.
+         //case: = value
          if (second == "") {      // case: =
-            print_line(title, line_num, "Show Everything and stuff...");
-         // If = is followed by a value, show the keys with that value.
+            print_line(title, line_num, "=\n");
+            curr = test.begin();
+            while (curr != test.end()) {
+               cout << curr->first << " = " << curr->second << endl;
+               ++curr;
+            }
+            // If = is followed by a value, show the keys with that value.
          } else {       // case: = value
-            print_line(title, line_num, "Find Keys from Values and stuff...");
+            print_line(title, line_num,
+                     "Find Keys from Values and stuff...");
          }
-      // If equals is not the first character...
+         // If equals is not the first character...
+         //cases key =, key = value
       } else {
          first = line.substr(0, equal_sign_pos);
          second = line.substr(equal_sign_pos + 1);
          first = trim(first);
          second = trim(second);
          // If there is a key followed by =, show the value of that key.
-         if (second == "") {    // case: key =
-            print_line(title, line_num, "Show Value for Key and stuff...");
-         // Final case is a key, =, and a value in that order.
-         // Insert the key and value into the map.
-         } else {    // case: key = value
+         // case: key =
+         if (second == "") {
+            print_line(title, line_num,
+                     "Show Value for Key and stuff...");
+            // Final case is a key, =, and a value in that order.
+            // Insert the key and value into the map.
+            // case: key = value
+         } else {
             str_str_pair pair(first, second);
-            test.insert(pair);
-            print_line(title, line_num, first + " = " + second);
+            curr = test.insert(pair);
+            print_line(title, line_num,
+                     curr->first + " = " + curr->second);
          }
       }
-      // Debugging prints. Remove later.
-      str_str_pair pair(first, second);
-      cout << pair << endl;
    }
 }
 
@@ -119,8 +123,9 @@ int main(int argc, char** argv) {
    sys_info::set_execname(argv[0]);
    scan_options(argc, argv);
    string line;
-   // If no arguments are specified when executing the program,
-   // the program will take cin inputs via command line.
+   str_str_map test;
+// If no arguments are specified when executing the program,
+// the program will take cin inputs via command line.
    if (argc == optind) {
       int line_num = 1;
       for (;;) {
@@ -136,13 +141,13 @@ int main(int argc, char** argv) {
                   <= line.find_first_not_of(" ")) {   // case: #
             continue;
          }
-         format_line("-", line_num, line);
+         format_line("-", line_num, line, test);
          ++line_num;
       }
    }
-   // If arguments are specified when executing the program,
-   // the program will treat the arguments as file names, and input the
-   // lines within those files as command inputs.
+// If arguments are specified when executing the program,
+// the program will treat the arguments as file names, and input the
+// lines within those files as command inputs.
    else {
       for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
          int line_num = 1;
@@ -161,7 +166,7 @@ int main(int argc, char** argv) {
                         <= line.find_first_not_of(" ")) {   // case: #
                   continue;
                }
-               format_line(*argp, line_num, line);
+               format_line(*argp, line_num, line, test);
                ++line_num;
             }
             myfile.close();
